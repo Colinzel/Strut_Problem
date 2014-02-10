87bf548feede5e911b67cd0dfd1fab11eea7decc
Colinzel <colinzelin@gmail.com>
2014-02-10 17:10:51 -0500

    Initial Commit

---
+++ Strut_Problem/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Strut_Problem
+//
+//  Created by Colin Zelin on 2/10/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Strut_ProblemTests/Strut_ProblemTests.m
@@ -0,0 +1,34 @@
+//
+//  Strut_ProblemTests.m
+//  Strut_ProblemTests
+//
+//  Created by Colin Zelin on 2/10/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Strut_ProblemTests : XCTestCase
+
+@end
+
+@implementation Strut_ProblemTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
